52f07496e028a0cfce2e0e8acf08500f96fe2782
Denitsa Slavkova <deni9309@gmail.com>
2024-02-22 22:10:45 +0200

    Connect app with remote API. Fetch weather info data by location;

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -1,16 +1,89 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+//import weatherInfo from './data'; //mock data for dev. and testing
 import './weather.css';
-import { useState } from 'react'
 
-const LocationSearch = () => {
-   
+const apiKey = import.meta.env.VITE_API_KEY;
+const baseUrl = import.meta.env.VITE_BASE_API_URL;
+
+const LocationSearch = ({ locationChange }) => {
+   const [location, setLocation] = useState('');
+
+   const handleSubmit = (e) => {    //we have to notify the 'weather' component that the form's been submitted
+      e.preventDefault();
+
+      locationChange(location);     //so using this function that we have as a prop, we pass the new 'location' value to the 'weather' component
+      setLocation('');    //then reset location's value to initial (to clear form input)
+   };
+
+   return (
+      <form className="location-search" onSubmit={handleSubmit}>
+         <input type="text"
+            value={location}
+            onChange={(event) => setLocation(event.target.value)}
+            placeholder="City, Country"
+            className="location-search-input"
+         />
+      </form>
+   );
 }
 
+const WeatherInfo = ({ weatherInfo }) => {
+   console.log(weatherInfo);
+   const celsiusTemperature = Math.floor(weatherInfo.main.temp - 273);
+   const celsiusFeelsLikeTemperature = Math.floor(weatherInfo.main.feels_like - 273);
+
+   return (
+      <div className='weather-info'>
+         <img src={`${weatherInfo.weather[0].icon}.png`}
+            className='weather-info-image'
+         />
+         <div className='weather-info-temp'>{celsiusTemperature} &#8451;</div>
+         <div className='weather-info-name'>{weatherInfo.name}</div>
+
+         <div className='weather-info-details'>
+            <div className='weather-info-detail'>
+               <b>Feels like:</b> {celsiusFeelsLikeTemperature} &#8451;
+            </div>
+            <div className='weather-info-detail'>
+               <b>Humidity:</b> {weatherInfo.main.humidity} %
+            </div>
+            <div className='weather-info-detail'>
+               <b>Wind speed:</b> {weatherInfo.wind.speed} km/h
+            </div>
+            <div className='weather-info-detail'>
+               <b>Pressure:</b> {weatherInfo.main.pressure} hPa
+            </div>
+         </div>
+      </div>
+   );
+};
+
 const Weather = () => {
-   const [count, setCount] = useState(0)
+   const [location, setLocation] = useState(null);
+   const [weatherInfo, setWeatherInfo] = useState(null);
+
+   useEffect(() => {
+      if (!location) return;
+
+      const url = `${baseUrl}?q=${location}&appid=${apiKey}`;
+
+      axios.get(url).then(response => {
+         setWeatherInfo(response.data);
+      })
+         .catch(res => {
+            setLocation(null);
+            setWeatherInfo(null);
+         });
+   }, [location]);
 
    return (
       <div className='weather-block'>
-         weather
+         <LocationSearch locationChange={(location) => setLocation(location)} />
+         
+         {weatherInfo &&
+            <WeatherInfo weatherInfo={weatherInfo} />
+         }
       </div>
    );
 }
--- src/data.js
+++ src/data.js
@@ -1,31 +1,35 @@
+// mine: bf0ae060b0b1672aa761d8b4e2b60d20
+//https://api.openweathermap.org/data/2.5/weather?lat=53.55&lon=10&appid=bf0ae060b0b1672aa761d8b4e2b60d20
+//https://api.openweathermap.org/data/2.5/weather?q=Hamburg&appid=bf0ae060b0b1672aa761d8b4e2b60d20
+
 /** For development use only */
 export default {
-   coord: { lon: 10, lat: 53.55 },
-   weather: [
-      { id: 804, main: "Clouds", description: "overcast clouds", icon: "04n" },
+   "coord": { "lon": 9.9953, "lat": 53.5453 },
+   "weather": [
+      { "id": 804, "main": "Clouds", "description": "overcast clouds", "icon": "04n" }
    ],
-   base: "stations",
-   main: {
-      temp: 283.9,
-      feels_like: 280.95,
-      temp_min: 283.6,
-      temp_max: 284.07,
-      pressure: 1004,
-      humidity: 84,
+   "base": "stations",
+   "main": {
+      "temp": 284.07,
+      "feels_like": 283.46,
+      "temp_min": 283.57,
+      "temp_max": 284.46,
+      "pressure": 981,
+      "humidity": 86
    },
-   visibility: 10000,
-   wind: { speed: 2.57, deg: 10 },
-   clouds: { all: 100 },
-   dt: 1704381631,
-   sys: {
-      type: 1,
-      id: 1263,
-      country: "DE",
-      sunrise: 1704353746,
-      sunset: 1704381188,
+   "visibility": 10000,
+   "wind": { "speed": 6.69, "deg": 150 },
+   "clouds": { "all": 100 },
+   "dt": 1708626184,
+   "sys": {
+      "type": 1,
+      "id": 1263,
+      "country": "DE",
+      "sunrise": 1708583131,
+      "sunset": 1708620121
    },
-   timezone: 3600,
-   id: 2911298,
-   name: "Hamburg",
-   cod: 200,
+   "timezone": 3600,
+   "id": 2911298,
+   "name": "Hamburg",
+   "cod": 200
 };
