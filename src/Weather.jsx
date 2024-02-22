562a60a55711ac5e7c5bde8725a938e0423e34fe
Denitsa Slavkova <deni9309@gmail.com>
2024-02-22 18:32:39 +0200

    Initial commit

---
+++ src/Weather.jsx
@@ -0,0 +1,18 @@
+import './weather.css';
+import { useState } from 'react'
+
+const LocationSearch = () => {
+   
+}
+
+const Weather = () => {
+   const [count, setCount] = useState(0)
+
+   return (
+      <div className='weather-block'>
+         weather
+      </div>
+   );
+}
+
+export default Weather;
